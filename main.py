07492efadd70e43773f2c4743fcbd8a4d8d8a6b6
oscar <luiwinghin8998@gmail.com>
2021-10-31 05:08:30 +0800

    new change

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from turtle import Turtle,Screen
 from paddle import Paddle
+from ball import Ball
+import time
+
 
 pingpong = Turtle()
 
@@ -12,14 +15,14 @@ pingpong_screen.bgcolor("black")
 pingpong_screen.tracer(0)
 
 user_paddle = Paddle(350,0)
-
 ai_paddle = Paddle(-350,0)
+
+new_ball = Ball()
+
+
 
 
 pingpong_screen.listen()
-
-
-
 
 pingpong_screen.onkeypress(user_paddle.moveup,"a")
 pingpong_screen.onkeypress(user_paddle.movedown,"d")
@@ -27,10 +30,28 @@ pingpong_screen.onkeypress(user_paddle.movedown,"d")
 pingpong_screen.onkeypress(ai_paddle.moveup,"q")
 pingpong_screen.onkeypress(ai_paddle.movedown,"e")
 
+
+
 game_on = True
 
+
+
 while game_on:
+    
     pingpong_screen.update()
+    
+    new_ball.move()
+
+    if new_ball.ycor() > 290 or new_ball.ycor() < -290:
+        new_ball.bounce(1,-1)
+    
+    if new_ball.xcor() > 390 or new_ball.xcor() < -390:
+        new_ball.bounce(-1,1)
+    
+    if new_ball.distance(user_paddle) <= 58 and new_ball.xcor() >= 340 :
+        new_ball.bounce(-1,1)
+
+    time.sleep(0.02)
 
 pingpong_screen.exitonclick()
 
